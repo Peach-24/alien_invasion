232e8e934d381326a1c5409f64908cf6f895bdb1
joshpeachey <josh.peachey1@nhs.net>
2024-02-05 21:45:40 +0000

    make aliens bounce left to right

--- settings.py
+++ settings.py
@@ -18,3 +18,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction being 1 means moving right, -1 means moving left
+        self.fleet_direction = 1
